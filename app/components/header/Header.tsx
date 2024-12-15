ff0eb2430dd7bdfffa884c4614b70e81d40e8615
Ahrorbek05 <akramjonovahrorbek96@gmail.com>
2024-12-15 14:58:06 +0500

    l

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+      <div>
+          <div className="container">
+              <nav>
+                  <ul>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/about">About</a></li>
+                      <li><a href="/contact">Contact</a></li>
+                  </ul>
+              </nav>
+          </div>
+    </div>
+  )
+}
